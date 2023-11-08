08437529ead4811c45a20d4437f3c95d45c3f012
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2023-11-08 22:11:51 +0100

    Remove duplicate p10k line found on .zshrc

--- aliases.zsh
+++ aliases.zsh
@@ -109,9 +109,6 @@ bri() {
   brew install $1
 }
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
 # Avoid accidental deletions by enabling interactive mode
 alias rm='rm -i'
 alias mv='mv -i'
