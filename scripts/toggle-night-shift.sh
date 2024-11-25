5d67a5a489f76fd554d319b666e18fe7672bba92
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-11-25 14:49:45 +0100

    Add Raycast script to toggle Night Shift

---
+++ scripts/toggle-night-shift.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Toggle Night Shift
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🔆
+# @raycast.packageName Toggle Night Shift
+
+# Documentation:
+# @raycast.description Toggles Night Shift On/Off using shortcuts
+# @raycast.author Giorgio Mendieta
+
+shortcuts run "Toggle Night Shift"
+echo "Night Shift Toggled"
+
