38f2c38f06e3b16f120f0c5fc362ad76f7dbac5f
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2026-05-16 13:23:15 +0200

    fix: Remove unused scripts

--- bin/immich-go_stack.sh
+++
@@ -1,6 +0,0 @@
-immich-go stack \
-  --server=http://192.168.1.100:2283 \
-  --api-key=nQ5RvVvFEsZPMnPkfv4rIbSEX0StkWjr9z6CtgRsNCE \
-  --manage-raw-jpeg=StackCoverJPG \
-  --manage-heic-jpeg=StackCoverJPG \
-  --manage-burst=Stack 
